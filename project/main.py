9268401bd7068de5957660541d86257ce1b5204f
Tobe <teokafor@gmail.com>
2022-11-07 22:53:13 -0600

    implemented duplicate song awareness

--- project/main.py
+++ project/main.py
@@ -1,7 +1,11 @@
+import os
+
 from bs4 import BeautifulSoup
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 import csv
+
+IGNORE_WRITE_TIME = 15  # If songs found are the same as the last entry and are under this value, then ignore write.
 
 def main():
     # The URL to gather data from
@@ -21,27 +25,51 @@ def main():
         # Collect necessary information
         song_title = raw_data.find(id='nowPlaying-title').string
         song_artist = raw_data.find(id='nowPlaying-artist').string
-        scrape_time = datetime.now().strftime("%m-%d-%Y %I:%M %p")
+        scrape_date = datetime.now().strftime("%m-%d-%Y")
 
-        song_data = [song_title, song_artist, scrape_time]
+        time_format = "%H:%M"
+        scrape_time = datetime.now().strftime(time_format)
+
+        song_data = [song_title, song_artist, scrape_date, scrape_time]
 
         # Print output
         print(f'Song Title:  {song_data[0]}')
         print(f'Song Artist: {song_data[1]}')
-        print(f'Scrape Time: {song_data[2]}')
+        print(f'Scrape Date: {song_data[2]}')
+        print(f'Scrape Time: {song_data[3]}')
 
         # Write contents to CSV file
-        song_output = open('song_output.csv', 'a')
-        writer = csv.writer(song_output, delimiter=',')
-        writer.writerow(song_data)
+        with open('song_output.csv', 'r+', newline='') as song_output:  # Open for reading and appending
+            writer = csv.writer(song_output, delimiter=',')
+            reader = csv.reader(song_output, delimiter=',')
 
-    # Close the file when we are done with it
-    song_output.close()
+            # Prevent the same song from being written to the file within a given timespan (e.g., 15 minutes)
+            try:
+                for line in reader:  # Jump to the last line
+                    last_line = line
+                # If it's the same song, by the same artist, on the same day, then compare times
+                if last_line[0] == song_data[0] and last_line[1] == song_data[1] and last_line[2] == song_data[2]:
+
+                    # Format date strings into datetime objects
+                    current_song_time = datetime.strptime(song_data[3], time_format)
+                    last_song_time = datetime.strptime(last_line[3], time_format)
+
+                    # Compare times to see if it's a duplicate
+                    time_delta_full = current_song_time - last_song_time
+                    time_delta_minutes = (int(time_delta_full.total_seconds() / 60))
+
+                    if time_delta_minutes <= IGNORE_WRITE_TIME:
+                        print(f'Song has not changed in {time_delta_minutes} minute(s). Ignoring write...')
+                else:  # If they aren't the same song (or it's been 15+ minutes), then write the data.
+                    writer.writerow(song_data)
+            except UnboundLocalError:  # If no data exists, then no preventative action is needed
+                pass
+
 
 if __name__ == "__main__":
     main()
 
-# TODO: Append to .CSV File
 # TODO: Spotify API stuff
 # TODO: Configurable settings (scrape freqeuncy, exclude dupes, timestamp output)
 # TODO: Run quietly in background
+# TODO: Fix weird accent marks in output (e.g., feliz navidad)
