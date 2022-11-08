e34cb5bb877f3d6de9fa5cef64f51e8aa560e103
Tobe <teokafor@gmail.com>
2022-11-07 19:46:45 -0600

    commented code

--- project/main.py
+++ project/main.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
+# The URL to gather data from
 resource = requests.get("https://listen.klove.com/Christmas")
 
-if resource.status_code != 200:
+# TODO: Make error reporting more verbose
+if resource.status_code != 200:  # This block will throw a generic error if the page cannot be returned for any reason.
     print("couldn't fetch page!")
     print('error code ' + resource.status_code)
     exit()
-else:
+else:  # If no error is found, proceed to scrape.
     content = resource.content
-    soup = BeautifulSoup(content, 'html.parser')
-    song_title = soup.find(id='nowPlaying-title')
-    song_artist = soup.find(id='nowPlaying-artist')
+    raw_data = BeautifulSoup(content, 'html.parser')
+    song_title = raw_data.find(id='nowPlaying-title')
+    song_artist = raw_data.find(id='nowPlaying-artist')
     print(song_title.string)
     print(song_artist.string)
 
