0cac8bb3d5207c66e802a2f33bfb8a599104a593
Tobe <teokafor@gmail.com>
2022-11-07 20:49:42 -0600

    added basic CSV output

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+import csv
 
 # The URL to gather data from
 resource = requests.get("https://listen.klove.com/Christmas")
@@ -11,14 +12,30 @@ if resource.status_code != 200:  # This block will throw a generic error if the 
     print('error code ' + resource.status_code)
     exit()
 else:  # If no error is found, proceed to scrape.
+
+    # Grab and format data
     content = resource.content
     raw_data = BeautifulSoup(content, 'html.parser')
-    song_title = raw_data.find(id='nowPlaying-title')
-    song_artist = raw_data.find(id='nowPlaying-artist')
+
+    # Collect necessary information
+    song_title = raw_data.find(id='nowPlaying-title').string
+    song_artist = raw_data.find(id='nowPlaying-artist').string
     scrape_time = datetime.now().strftime("%m-%d-%Y %I:%M %p")
-    print(f'Song Title:  {song_title.string}')
-    print(f'Song Artist: {song_artist.string}')
-    print(f'Scrape Time: {scrape_time}')
+
+    song_data = [song_title, song_artist, scrape_time]
+
+    # Print output
+    print(f'Song Title:  {song_data[0]}')
+    print(f'Song Artist: {song_data[1]}')
+    print(f'Scrape Time: {song_data[2]}')
+
+    # Write contents to CSV file
+    song_output = open('song_output.csv', 'a')
+    writer = csv.writer(song_output, delimiter=',')
+    writer.writerow(song_data)
+
+# Close the file when we are done with it
+song_output.close()
 
 # TODO: Append to .CSV File
 # TODO: Spotify API stuff
